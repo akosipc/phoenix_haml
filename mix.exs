57bc1b56ea308fff69ced2bc0062db29a62a04b2
Adrian Peterson Co <adrianpco@gmail.com>
2016-04-30 14:01:30 +0800

    Updates phoenix_haml for phoenix 1.2.0-rc

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule PhoenixHaml.Mixfile do
 
   defp deps do
     [
-      {:phoenix, "~> 1.1"},
+      {:phoenix, "~> 1.2.0-rc"},
       {:phoenix_html, "~> 2.3"},
       {:calliope, "~> 0.4.0"}
     ]
